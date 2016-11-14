10dd7d22277d772f03f54d497add5cb4ba7a57ea
Namanda Musoke <namanda@NamandasokesMBP.attlocal.net>
2016-11-13 23:15:29 -0600

    created game result alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,14 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	window.alert ("You found a match!")
+} else if (cardThree === cardFour) {
+	window.alert ("You found a match!")
+} else if (cardTwo === cardThree){
+	window.alert ("Sorry, try again.")
+} else if (cardOne ===cardFour) {
+	window.alert ("Sorry, try again.")
+}
+console.log (result);
